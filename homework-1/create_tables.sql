9ea9279d36822fdbadbf0e993dc6d21bc942ee46
nvdovin <narmunine@gmail.com>
2023-05-30 01:10:15 +0300

    Производится запись в БД корректно

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+	customers_id varchar(5) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+CREATE TABLE employees
+(
+	first_name varchar(100) PRIMARY KEY,
+	last_name varchar(100),
+	title varchar(100),
+	birth_date varchar(10),
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) UNIQUE REFERENCES customers(customers_id) NOT NULL,
+	employee_id int,
+	order_date varchar(10),
+	ship_city varchar(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,58 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+import os
+
+
+class PostgresData:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host="localhost",
+            database="north",
+            user="postgres",
+            password=os.getenv("DB_PASSWORD")
+        )
+        self.cur = self.conn.cursor()
+        self.csv_data = ["customers_data", "employees_data", "orders_data"]
+
+    @staticmethod
+    def get_csv_data(file_name):
+        """ Получаем данные из файлов формата .csv """
+
+        csv_file = open(f"north_data/{file_name}.csv", "r", encoding="utf-8", newline="")
+        return csv.reader(csv_file, delimiter=",", quotechar='"')
+
+    def write_to_database(self, database):
+        """ Записываем данные в базу данных """
+
+        database_name = database.split("_")[0]
+        csv_data = self.get_csv_data(database)
+
+        with self.conn as conn:
+            with conn.cursor() as cur:
+                next(csv_data)
+                for items in csv_data:
+                    row = []
+                    for item in items:
+                        if len(item) == 1:
+                            row.append(item)
+                        elif len(item) > 1:
+                            row.append("'" + str(item.replace("'", "`")) + "'")
+
+                    cur.execute(f"""INSERT INTO {database_name} VALUES
+({(', '.join(row))});""")
+
+        cur.close()
+
+    def main_function(self):
+        counter = 1
+        for database in self.csv_data:
+            self.write_to_database(database)
+            print(f"{counter}")
+            counter += 1
+
+
+
+pg = PostgresData()
+pg.main_function()
