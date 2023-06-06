d5fb43c9aad6049a465a86bff9ec9538bf178fec
nvdovin <narmunine@gmail.com>
2023-06-06 15:18:12 +0300

    Готовое 3 задание.

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,49 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT DISTINCT(customers.company_name) AS customer, 
+CONCAT(first_name, ' ', last_name) AS employee
+FROM customers
+INNER JOIN employees ON customers.contact_title=employees.title
+LEFT JOIN orders USING(customer_id)
+LEFT JOIN shippers ON orders.ship_via=shippers.shipper_id
+WHERE customers.city='London' AND employees.city='London' 
+AND shippers.company_name='United Package'
+--Да, я знаю, что на скриншоте чуть-чуть не так, но увы, друзья, я смог только так((
+
+
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+INNER JOIN suppliers USING(supplier_id)
+LEFT JOIN categories USING(category_id)
+WHERE discontinued <> 1 AND units_in_stock < 25 AND category_name IN ('Dairy Products', 'Condiments')
+ORDER BY units_in_stoc
+
+
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name 
+FROM customers
+WHERE customer_id NOT IN(SELECT customer_id FROM orders)
+
+
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
--- Этот запрос написать именно с использованием подзапроса.
+-- Этот запрос написать именно с использованием подзапроса
+
+SELECT DISTINCT(product_name)
+FROM products
+WHERE product_id IN(
+	SELECT product_id
+	FROM order_details
+	WHERE quantity=10
+)
+
+
